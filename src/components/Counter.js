4be21f0a04f7e586dafc82e673cf8a5111c27c9a
unknown <manish9864040418@gmail.com>
2024-12-17 22:36:04 +0530

    first commit

---
+++ src/components/Counter.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+
+  const handleDecrement = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div>
+      <button onClick={handleIncrement}>Increment</button>
+      <div>Count: {count}</div>
+      <button onClick={handleDecrement}>Decrement</button>
+    </div>
+  );
+};
+
+export default Counter;
