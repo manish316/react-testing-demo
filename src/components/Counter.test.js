276c810a75da606cb039668f5dee4c56e0ebefca
unknown <manish9864040418@gmail.com>
2024-12-18 10:30:10 +0530

    added describe and it methodu using skip and only versions

--- src/components/Counter.test.js
+++ src/components/Counter.test.js
@@ -1,8 +1,23 @@
 import { render, screen } from "@testing-library/react";
 import Counter from "./Counter";
 
-test("Counter should render correctly ", () => {
-  render(<Counter />);
-  const countElement = screen.getByText(/count/i);
-  expect(countElement).toBeInTheDocument();
+describe("Counter", () => {
+  test("should render correctly ", () => {
+    render(<Counter />);
+    const countElement = screen.getByText(/count/i);
+    expect(countElement).toBeInTheDocument();
+  });
+});
+describe("Counter Buttons", () => {
+  test("Increment Button should render fine", () => {
+    render(<Counter />);
+    const IncrementButton = screen.getByRole("button", { name: "Increment" });
+    expect(IncrementButton).toBeInTheDocument();
+  });
+
+  it("Decrement Button should render fine", () => {
+    render(<Counter />);
+    const decrementButton = screen.getByRole("button", { name: "Decrement" });
+    expect(decrementButton).toBeInTheDocument();
+  });
 });
--- src/components/Header.spec.js
+++ src/components/Header.spec.js
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import Header from "./Header";
 
-test("Counter should render correctly ", () => {
+test("Header should render correctly ", () => {
   render(<Header />);
   const countElement = screen.getByText(/header/i);
   expect(countElement).toBeInTheDocument();
