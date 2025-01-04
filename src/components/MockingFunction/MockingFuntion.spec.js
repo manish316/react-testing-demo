0303c7d0809cd88221d3b18d0b716c1ab3c2495e
unknown <manish9864040418@gmail.com>
2025-01-04 23:04:49 +0530

    upadte

--- src/components/MockingFunction/MockingFuntion.spec.js
+++ src/components/MockingFunction/MockingFuntion.spec.js
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import MockingFunction from "./MockingFunction/MockingFunction";
+import MockingFunction from "./MockingFunction";
 
 describe("Testing Counter Component using mocking function", () => {
   const handleIncrement = jest.fn();
